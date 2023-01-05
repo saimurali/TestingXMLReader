1beeccc2e17e03aa65d998cff035f3ef3411ba4a
SaiMakala <SaiMakala@MSI>
2023-01-05 20:30:35 +0530

    Add project files.

---
+++ TestingXMLReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestingXMLReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start with XmlReader object  
+            //here, we try to setup Stream between the XML file nad xmlReader  
+            using (XmlReader reader = XmlReader.Create(@"C:\Users\SaiMakala\source\repos\TestingXMLReader\TestingXMLReader\Data.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        //return only when you have START tag  
+                        switch (reader.Name.ToString())
+                        {
+                            case "Name":
+                                Console.WriteLine("Name of the Element is : " + reader.ReadString());
+                                break;
+                            case "Location":
+                                Console.WriteLine("Your Location is : " + reader.ReadString());
+                                break;
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
